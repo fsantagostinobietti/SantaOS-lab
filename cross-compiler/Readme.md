df0e69219bb8aac017a91725dd0282db155062a8
fsantagostinobietti <fabio.santagostinobietti@unicredit.eu>
2017-05-09 16:50:45 +0200

    downloaded from internet

---
+++ cross-compiler/Readme.md
@@ -0,0 +1,7 @@
+GCC cross-compiler
+------------------
+
+Used to produce binary executables that are supposed to run on bare-metal (or VM). 
+
+Downloaded from:
+    ???
---
+++ cross-compiler/lib/gcc/i386-linux/4.6.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/tony/buildall/src/gcc/configure --target=i386-linux --host=x86_64-linux-gnu --build=x86_64-linux-gnu --enable-targets=all --prefix=/opt/cross/gcc-4.6.3-nolibc/i386-linux/ --enable-languages=c --with-newlib --without-headers --enable-sjlj-exceptions --with-system-libunwind --disable-nls --disable-threads --disable-shared --disable-libmudflap --disable-libssp --disable-libgomp --disable-decimal-float --enable-checking=release --with-mpfr=/home/tony/buildall/src/sys-x86_64 --with-gmp=/home/tony/buildall/src/sys-x86_64 --disable-bootstrap --disable-libquadmath";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu_32", "i386" }, { "cpu_64", "generic" }, { "arch_32", "i386" }, { "arch_64", "x86-64" } };
